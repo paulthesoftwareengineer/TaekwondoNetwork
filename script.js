21d1e108e15a6daa9d49fd2ac7b86282b24700ea
Paul Turner <baldheader@live.com>
2023-07-26 16:49:47 -0700

    added google map

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ body {
     height: 15vw;
     object-fit: cover;
   }
+
+  function initMap() {
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(40.7128, -74.0060),  // New York coordinates
+      mapTypeId: 'roadmap'
+    };
+  
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+  
   
